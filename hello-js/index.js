18c974ce5011e43731f4600ab6ad9c495c9534f3
rolniuq <truongquynh2525@gmail.com>
2026-03-20 15:12:26 +0700

    feat: update something

--- hello-js/index.js
+++ hello-js/index.js
@@ -1,8 +1,19 @@
 exports.handler = async (event) => {
-  console.log('Received event:', JSON.stringify(event, null, 2));
+  console.log("Received event:", JSON.stringify(event, null, 2));
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from Lambda!' }),
+    body: JSON.stringify({ message: "Hello from Lambda!" }),
+  };
+};
+
+exports.handler2 = async (event) => {
+  // cause event.body is a string
+  const body = JSON.parse(event.body);
+  console.log("Received body: ", body);
+
+  return {
+    statusCode: 200,
+    body: event.body,
   };
 };
